55a80ea9f8251822a052f14091a55302c7cc539b
AnthonyScott <anthony.scott1005@gmail.com>
2023-07-14 13:38:13 -0400

    Initial commit

---
+++ permutations.js
@@ -0,0 +1,15 @@
+
+
+
+function getPermutations(str) {
+    if (!str || typeof str !== "string") return "Please enter a string";
+    else if (str.length <  2) return str;
+
+    let output = [];
+
+    for (let i = 0; i < str.length; i++) {
+
+    }
+
+    return output;
+}
